7b1116b9ea2ce4a8121794344a0b48e4ded32b25
Kaylee Hartzog <kayleehartzog@Kaylees-MacBook-Pro.local>
2019-05-14 10:43:26 -0600

    Kaylee says hi

--- src/HelloFrom/Hello.java
+++ src/HelloFrom/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Rachel");
+
+        System.out.println("Hello Rachel, my name is Kaylee. I think that you are awesome and super smart!!!");
     }
 }
